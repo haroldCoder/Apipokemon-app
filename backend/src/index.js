06bffe6e52d72d2dff5677794014e0470ac809a2
koderx <haroldc2005@gmail.com>
2023-01-26 18:20:44 -0500

    post db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
+const bodyParser = require("body-parser");
+const conection = require("./DB/conection")
 
 app.set("port", 8000);
-app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
 
 app.use("/api/pokemon", require("./routes/pokemon.route"))
 
 app.listen(app.get("port"), (req, res)=>{
     console.log(`server on port ${app.get("port")}`);
+    conection;
 })
